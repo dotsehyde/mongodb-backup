55f85992e4ece2d817dd41102e9c948287cc2540
Benjamin <dotsehyde@gmail.com>
2023-11-26 19:21:33 +0000

    🦄 refactor: Major update

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const backup = require('mongodb-backup');
+const { MongooseBackup } = require("mongoose-backup");
+const Backup = new MongooseBackup({
+  url: "mongodb://localhost:27017/test",
+});
 
-const backupOptions = {
-  uri: 'mongodb://mongo:27017/your-database-name', // Update with your MongoDB connection URI
-  root: __dirname,
-  parser: 'json',
-};
+Backup.on('connected', () => {
+  Backup.Localize({
+      per: 'monthly' // minutes, hours, daily, monthly, yearly
+  });
+  Backup.Github({
+    key: "ghp_XXXX",
+    per: "yearly",
+    options: {
+        owner: "username",
+        repo: "repo",
+    }
+});
+});
+Backup.on("ping", (data) => {
+  console.log(`${data.time}: Mongoose Backup is alive. Time: `);
+});
 
-backup.dump(backupOptions, (err) => {
-  if (err) {
-    console.error('Backup failed:', err);
-  } else {
-    console.log('Backup successful');
-  }
+Backup.on("githubBackup", (data) => {
+  console.log(`${data.time}: [GitHub]: Total ${data.total} documents with ${data.items} items backed up.`);
+});
+
+Backup.on("localizeBackup", (data) => {
+  console.log(`${data.time}: [Local]: Total ${data.total} documents with ${data.items} items backed up.`);
 });
